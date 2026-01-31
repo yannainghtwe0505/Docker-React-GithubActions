07269cbd16df62ca9d8526b6f2156289b675c4fb
Zalatpan123[0]=? <ynainghtwe61@gmail.com>
2026-02-01 00:10:04 +0900

    change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, I am Yannainghtwe, Thank you!
         </a>
       </header>
     </div>
